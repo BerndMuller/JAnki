288155f6b582cf2978d89574264cdc370218ec69
Bernd Mueller <bernd.mueller@ostfalia.de>
2017-12-08 17:50:05 +0100

    README.md added

---
+++ README.md
@@ -0,0 +1,33 @@
+# JAnki
+
+A simple library to use Anki Overdrive with Java
+
+## Getting Started
+
+The class ``de.pdbm.janki.core.Vehicle`` is JAnki's public API and should be your starting point.
+
+### Prerequisites
+
+JAnki uses TinyB <https://github.com/intel-iot-devkit/tinyb> which you have to install.
+Please ensure that you have read the troubleshooting guide of TinyB.
+
+TinyB uses BlueZ, a Linux Bluetooth stack, which you also have to install. Usually BlueZ is included in your Linux distribution.
+
+### Installing
+
+Since we use Maven simple do
+
+```
+mvn clean package
+```
+
+or
+
+```
+mvn clean install
+```
+
+## License
+
+ich überlege noch
+
--- src/main/java/de/pdbm/janki/core/package-info.java
+++ src/main/java/de/pdbm/janki/core/package-info.java
@@ -3,26 +3,7 @@
  * JAnki is a simple library to use Anki Overdrive with Java.
  * 
  * <p>
- * JAnki uses <a href="https://github.com/intel-iot-devkit/tinyb" target="_blank">TinyB</a>
- * which you have to install.
- * 
- * <p>
- * TinyB uses BlueZ, a Linux Bluetooth stack, which you also have to install. Usually
- * included in your Linux distribution.
- * 
- * <p>
- * If you have installed TinyB carefully, you have been followed the following advice:
- * 
- * <blockquote>
- * TinyB requires CMake 3.1+ for building and requires GLib/GIO 2.40+.  
- * It also requires BlueZ with GATT profile activated, which is currently  
- * experimental (as of BlueZ 5.37), so you might have to run bluetoothd  
- * with the -E flag. For example, on a system with systemd (Fedora, poky, etc.)  
- * edit the bluetooth.service file (usually found in /usr/lib/systemd/system/  
- * or /lib/systemd/system) and append -E to ExecStart line, restart the  
- * daemon with systemctl restart bluetooth. 
- * </blockquote>
- * 
+ * The class Vehicle is your entry point to use JAnki. 
  * 
  * @author bernd
  *
